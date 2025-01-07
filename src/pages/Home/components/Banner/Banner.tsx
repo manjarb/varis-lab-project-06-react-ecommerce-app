40f2d9f75e17ed5d5b66d75c8011a3b10f287273
Varis Darasirikul <manjarb4@gmail.com>
2025-01-08 00:18:07 +0700

    second commit

--- src/pages/Home/components/Banner/Banner.tsx
+++ src/pages/Home/components/Banner/Banner.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
 
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+import "swiper/swiper-bundle.css";
 
 const Banner: React.FC = () => {
   const banners = [
